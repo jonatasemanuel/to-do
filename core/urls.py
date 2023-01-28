6ea8b4f72e636782e5c8a987579a4ac0cbc465f4
Jonatas Silva <jonatassilvaemanuel@gmail.com>
2023-01-28 11:55:19 -0300

    Update class config

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns=[
     #path('', views.new_task, name='new_task'),
     path('', views.HomeFormView.as_view(), name='home'),
-    path('edit/<int:pk>',views.edit, name='edit'),
+    path('edit/<int:pk>',views.TaskUpdateView.as_view(), name='edit'),
     path('<int:pk>/delete', views.delete, name='delete'),
 ]
  
--- core/views.py
+++ core/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from .forms import TaskForm
 from .models import Task
 from django.views.generic import FormView, UpdateView
 # from django.contrib.auth.decorators import login_required
 
-"""
-def new_task(request):
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form = form.save(commit=False)
-            #new_task.owner = request.user
-            form.save()
-            form = TaskForm()
-
-    form = TaskForm()
-    tasks = Task.objects.order_by('date_added').all()
-    context = {'form': form, 'tasks': tasks}
-    return render(request, 'core/home.html', context)"""
 
 class HomeFormView(FormView):
     template_name = 'core/home.html'
@@ -37,6 +24,41 @@ class HomeFormView(FormView):
         return super(HomeFormView, self).form_invalid(form, *args, **kwargs)
 
 
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'core/update.html'
+    fields = ['task']
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def delete(request, pk):
+    task = Task.objects.get(id=pk)
+    
+    if request.method == 'POST':
+        task.delete()
+        return redirect('home')
+
+    context = {'task': task}
+    return render(request, 'core/delete.html', context)
+
+
+"""
+def new_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            #new_task.owner = request.user
+            form.save()
+            form = TaskForm()
+
+    form = TaskForm()
+    tasks = Task.objects.order_by('date_added').all()
+    context = {'form': form, 'tasks': tasks}
+    return render(request, 'core/home.html', context)
+    
 def edit(request, pk):
     task = Task.objects.get(id=pk)
     form = TaskForm(instance=task)
@@ -50,13 +72,4 @@ def edit(request, pk):
     context = {'form': form}
     return render(request, 'core/update.html', context)
 
-
-def delete(request, pk):
-    task = Task.objects.get(id=pk)
-    
-    if request.method == 'POST':
-        task.delete()
-        return redirect('home')
-
-    context = {'task': task}
-    return render(request, 'core/delete.html', context)
+"""
