664619a0fb474b73ded51e4ba9c89c76b13531d1
Jonatas Silva <jonatassilvaemanuel@gmail.com>
2023-02-01 23:52:59 -0300

    icon and a little bit more of css

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Task(models.Model):
     """Campo para o user colocar uma tarefa"""
-    task = models.CharField(max_length=65)
+    task = models.CharField(max_length=100)
     complete = models.BooleanField(default=False)
     date_added = models.DateField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
